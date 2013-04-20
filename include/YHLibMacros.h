4d2c189d61958b49adfe4627c9fc3002df269c45
trarck <trarck@gmail.com>
2013-04-20 12:58:29 +0800

    add include file

---
+++ include/YHLibMacros.h
@@ -0,0 +1,10 @@
+#ifndef COCOS_YHLIB_MACROS_H_
+#define COCOS_YHLIB_MACROS_H_
+
+#define NS_CC_YHLIB_BEGIN                     namespace cocos2d { namespace yhlib {
+#define NS_CC_YHLIB_END                       }}
+#define USING_NS_CC_YHLIB                     using namespace cocos2d::yhlib
+
+
+#endif //COCOS_YHLIB_MACROS_H_
+
---
+++ include/cocos-yhlib.h
@@ -0,0 +1,7 @@
+#ifndef COCOS_YHLIB_H_
+#define COCOS_YHLIB_H_
+
+#include "YHLibMacros.h"
+
+
+#endif //COCOS_YHLIB_H_
