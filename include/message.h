44e42b8c2cc7ff847be964762afca2470f46a877
trarck <trarck@gmail.com>
2013-04-20 12:58:15 +0800

    remove old project file

--- include/message.h
+++
@@ -1,9 +0,0 @@
-#ifndef __YH_MESSAGE_H__
-#define __YH_MESSAGE_H__
-
-#include "CCMessage.h"
-#include "CCMessageHandler.h"
-#include "CCCompleteMessageManager.h"
-#include "CCSimpleMessageManager.h"
-
-#endif //__YH_MESSAGE_H__
